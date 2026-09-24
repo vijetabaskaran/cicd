5e5c57e87f32d828a3f359299787247a33fc17b1
vijetabaskaran <vijeta9c@gmail.com>
2026-09-24 11:31:52 +0530

    Test CI build failure

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
-          <h2>Connect with us</h2>
+          <h2>Connect with us</h2
           <p>Join the Vite community</p>
           <ul>
             <li>
